b92545dd124f809496c00787d6591de080d7f89b
Pweetybars <vadhlcx@gmail.com>
2023-09-11 18:14:00 +0700

    More changes

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
-const about = document.querySelector('header');
+//* make it a fixed header
+const header = document.querySelector('header')
 
-height = about.getBoundingClientRect().height;
 
-console.log(height);
+window.addEventListener('scroll', ()=>{
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > 70){
+        header.classList.add('show-link');
+    } else { 
+        header.classList.remove('show-link')
+    }
+
+})
+
+
+//* link sections 
+const links = document.querySelectorAll(".link");
+
+links.forEach(function(link){
+    link.addEventListener("click", function(e){
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute("id").slice(1);
+        const element = document.getElementById(id);
+
+        const headerHeight = header.getBoundingClientRect().height; 
+        const showlink = header.classList.contains('show-link');
+        let position = element.offsetTop;
+
+        if (!showlink){
+            position = position-headerHeight;
+        }
+
+        position = position - 100;
+
+        window.scrollTo({
+            left:0, 
+            top: position,
+        });
+
+    })
+})
+
+
+const topLink = document.querySelector('.top-link');
+console.log(topLink);
+
+window.addEventListener('scroll', ()=>{
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > 70){
+        topLink.classList.add("scroll-link")
+    } else { 
+        topLink.classList.remove("scroll-link")
+    }
+
+})
