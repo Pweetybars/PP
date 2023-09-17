419521f82c11d6c195653003d3bbf0cccb256ba8
Pweetybars <vadhlcx@gmail.com>
2023-09-17 17:04:07 +0700

    more changes

--- ffc/players/app.js
+++ ffc/players/app.js
@@ -1,5 +1,5 @@
-const player = [
-    player1 = {
+const players = [
+    {
         name : "Sieng Chanthea", 
         birthdate : "9/September/2002", 
         team : "Al Shahaniya",
@@ -7,7 +7,7 @@ const player = [
         goal : 5, 
         role : "Winger"
     }, 
-    player2 = {
+    {
         name : "Soeuy Visal", 
         birthdate : "19/August/1995",
         team : "Preah Khan Reach Svay Rieng",
@@ -15,7 +15,7 @@ const player = [
         goal : 5, 
         role : "Center Back"
     },
-    player3 = {
+    {
         name : "Sin Sophanat",
         birthdate : "20/April/1997",
         team : "Visakha Football Club",
@@ -23,7 +23,7 @@ const player = [
         goal : 1, 
         role : "Center Back"
     }, 
-    player4 = { 
+    { 
         name : "Choun Chanchav", 
         birthdate : "5/May/1999",
         team : "Phnom Penh Crown Football Club",
@@ -31,7 +31,7 @@ const player = [
         goal : 2, 
         role : "Center Back" 
     },
-    player5 = {
+    {
         name : "Kan Pisal",
         birthdate : "9/August/1998",
         team : "Boeung Ket Football Club", 
@@ -39,7 +39,7 @@ const player = [
         goal : 0, 
         role : "Left Back"
     }, 
-    player6 = {
+    {
         name : "Vann Tailamey", 
         birthdate: "2/April/1999",
         team : "Boueng Ket Football Club",
@@ -47,7 +47,7 @@ const player = [
         goal : 0, 
         role : "Right Back"
     },
-    player7  = {
+    {
         name : "Kouch Sokumpheak",
         birthdate : "15/February/1987",
         team : "Nagaworld Football Club",
@@ -55,7 +55,7 @@ const player = [
         goal : 7, 
         role : "Midfielder"
     },
-    player8 = {
+    {
         name : "Orn Chanpolin",
         birthdate : "15/March/1998",
         team : "Phnom Penh Crown Football Club",
@@ -63,7 +63,7 @@ const player = [
         goal : 1, 
         role : "Defensive Midfielder"
     },
-    player9 = {
+    {
         name : "Sin Kakada", 
         birthdate : "29/July/2000",
         team : "Visakha Football Club", 
@@ -71,7 +71,7 @@ const player = [
         goal : 0, 
         role : "Attacking Midfielder"
     }, 
-    player10 = {
+    {
         name : "Lim Pisoth",
         birthdate : "29/August/2001",
         team : "Phnom Penh Crown Football Club",
@@ -79,7 +79,7 @@ const player = [
         goal : 2, 
         role : "Winger"
     },
-    player11 = {
+    {
         name : "Hul Kimhuy", 
         birthdate : "7/April/2000",
         team : "Visakha Football Club",
@@ -87,7 +87,7 @@ const player = [
         cleansheet : 0, 
         role : "Goalkeeper"
     },
-    player12 = {
+    {
         name : "Keo Soksela",
         birthdate : "1/August/1997",
         team : "Visakha Football Club",
@@ -95,7 +95,7 @@ const player = [
         cleansheet : 0, 
         role : "Goalkeeper"
     },
-    player13 = {
+    {
         name : "Chhong Bunnath",
         birthdate : "28/November/1998",
         team : "Nagaworld",
@@ -103,7 +103,7 @@ const player = [
         goal : 0, 
         role : "Center Back"
     },
-    player14 = {
+    {
         name : "Kouch Dani",
         birthdate : "11/October/1990",
         team : "Preah Khan Reach Svay Rieng",
@@ -111,7 +111,7 @@ const player = [
         goal : 0, 
         role : "Midfielder"
     }, 
-    player15 = {
+    {
         name : "Khoan Soben",
         birthdate : "4/October/2004",
         team : "Boeung Ket Football Club",
@@ -119,7 +119,7 @@ const player = [
         goal : 0, 
         role : "Center Forward"
     },
-    player16 = {
+    {
         name : "Chantha Chantheaka",
         birthdate : "29/September/2000",
         team : "Boeung Ket Football Club",
@@ -127,7 +127,7 @@ const player = [
         goal : 0, 
         role : "Left Winger"
     },
-    player17 = {
+    {
         name : "Nick Taylor",
         birthdate : "2/September/1998",
         team : "Preah Khan Reach Svay Rieng",
@@ -135,7 +135,7 @@ const player = [
         goal : 1, 
         role : "Left Winger" 
     },
-    player18 = {
+    {
         name : "Yeu Muslim",
         birthdate : "25/December/1998",
         team : "Phnom Penh Crown Football Club",
@@ -143,7 +143,7 @@ const player = [
         goal : 0,
         role : "Right Midfielder"
     },
-    player19 = {
+    {
         name : "Chheng Meng",
         birthdate : "27/February/1998",
         team : "Visakha Football Club",
@@ -151,7 +151,7 @@ const player = [
         goal : 0, 
         role : "Left Back"
     },
-    player20 = {
+    {
         name : "Ouk Sovann",
         birthdate : "15/May/1998",
         team : "Visaka Football Club",
@@ -159,7 +159,7 @@ const player = [
         goal : 0, 
         role : "Center Back"
     },
-    player21 = {
+    {
         name : "Reung Bunheing",
         birthdate : "25/September/1992",
         team : "Visakha Football Club",
@@ -168,3 +168,13 @@ const player = [
         role : "Center Forward"
     }
 ]
+
+let playerHTML = " ";
+players.forEach((player)=>{
+    playerHTML += `<div class="player-container">
+    <img class="player-img" src="player picture/${player.name}.jpg" alt="">
+    <p>${player.name}</p>
+</div>`
+})
+
+document.querySelector('.main-container').innerHTML = playerHTML;
