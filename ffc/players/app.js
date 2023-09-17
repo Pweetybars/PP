aa7c86a05ad2fcc2c74240b384c7cf82905dd60d
Pweetybars <vadhlcx@gmail.com>
2023-09-17 16:13:54 +0700

    upload player photo

---
+++ ffc/players/app.js
@@ -0,0 +1,170 @@
+const player = [
+    player1 = {
+        name : "Sieng Chanthea", 
+        birthdate : "9/September/2002", 
+        team : "Al Shahaniya",
+        appearance : 28, 
+        goal : 5, 
+        role : "Winger"
+    }, 
+    player2 = {
+        name : "Soeuy Visal", 
+        birthdate : "19/August/1995",
+        team : "Preah Khan Reach Svay Rieng",
+        appearance : 74, 
+        goal : 5, 
+        role : "Center Back"
+    },
+    player3 = {
+        name : "Sin Sophanat",
+        birthdate : "20/April/1997",
+        team : "Visakha Football Club",
+        appearance : 3, 
+        goal : 1, 
+        role : "Center Back"
+    }, 
+    player4 = { 
+        name : "Choun Chanchav", 
+        birthdate : "5/May/1999",
+        team : "Phnom Penh Crown Football Club",
+        appearance : 15, 
+        goal : 2, 
+        role : "Center Back" 
+    },
+    player5 = {
+        name : "Kan Pisal",
+        birthdate : "9/August/1998",
+        team : "Boeung Ket Football Club", 
+        appearance : 3, 
+        goal : 0, 
+        role : "Left Back"
+    }, 
+    player6 = {
+        name : "Vann Tailamey", 
+        birthdate: "2/April/1999",
+        team : "Boueng Ket Football Club",
+        appearance : 1,
+        goal : 0, 
+        role : "Right Back"
+    },
+    player7  = {
+        name : "Kouch Sokumpheak",
+        birthdate : "15/February/1987",
+        team : "Nagaworld Football Club",
+        appearance : 68, 
+        goal : 7, 
+        role : "Midfielder"
+    },
+    player8 = {
+        name : "Orn Chanpolin",
+        birthdate : "15/March/1998",
+        team : "Phnom Penh Crown Football Club",
+        appearance : 34, 
+        goal : 1, 
+        role : "Defensive Midfielder"
+    },
+    player9 = {
+        name : "Sin Kakada", 
+        birthdate : "29/July/2000",
+        team : "Visakha Football Club", 
+        appearance : 8, 
+        goal : 0, 
+        role : "Attacking Midfielder"
+    }, 
+    player10 = {
+        name : "Lim Pisoth",
+        birthdate : "29/August/2001",
+        team : "Phnom Penh Crown Football Club",
+        appearance : 17, 
+        goal : 2, 
+        role : "Winger"
+    },
+    player11 = {
+        name : "Hul Kimhuy", 
+        birthdate : "7/April/2000",
+        team : "Visakha Football Club",
+        appearance : 11,
+        cleansheet : 0, 
+        role : "Goalkeeper"
+    },
+    player12 = {
+        name : "Keo Soksela",
+        birthdate : "1/August/1997",
+        team : "Visakha Football Club",
+        appearance : 22, 
+        cleansheet : 0, 
+        role : "Goalkeeper"
+    },
+    player13 = {
+        name : "Chhong Bunnath",
+        birthdate : "28/November/1998",
+        team : "Nagaworld",
+        appearance : 3,
+        goal : 0, 
+        role : "Center Back"
+    },
+    player14 = {
+        name : "Kouch Dani",
+        birthdate : "11/October/1990",
+        team : "Preah Khan Reach Svay Rieng",
+        appearance : 10, 
+        goal : 0, 
+        role : "Midfielder"
+    }, 
+    player15 = {
+        name : "Khoan Soben",
+        birthdate : "4/October/2004",
+        team : "Boeung Ket Football Club",
+        appearance : 1, 
+        goal : 0, 
+        role : "Center Forward"
+    },
+    player16 = {
+        name : "Chantha Chantheaka",
+        birthdate : "29/September/2000",
+        team : "Boeung Ket Football Club",
+        appearance : 1, 
+        goal : 0, 
+        role : "Left Winger"
+    },
+    player17 = {
+        name : "Nick Taylor",
+        birthdate : "2/September/1998",
+        team : "Preah Khan Reach Svay Rieng",
+        appearance : 7, 
+        goal : 1, 
+        role : "Left Winger" 
+    },
+    player18 = {
+        name : "Yeu Muslim",
+        birthdate : "25/December/1998",
+        team : "Phnom Penh Crown Football Club",
+        appearance : 9,
+        goal : 0,
+        role : "Right Midfielder"
+    },
+    player19 = {
+        name : "Chheng Meng",
+        birthdate : "27/February/1998",
+        team : "Visakha Football Club",
+        appearance : 28,
+        goal : 0, 
+        role : "Left Back"
+    },
+    player20 = {
+        name : "Ouk Sovann",
+        birthdate : "15/May/1998",
+        team : "Visaka Football Club",
+        appearance : 10, 
+        goal : 0, 
+        role : "Center Back"
+    },
+    player21 = {
+        name : "Reung Bunheing",
+        birthdate : "25/September/1992",
+        team : "Visakha Football Club",
+        appearance : 16, 
+        goal : 4, 
+        role : "Center Forward"
+    }
+]
